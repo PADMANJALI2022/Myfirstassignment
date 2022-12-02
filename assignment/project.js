2316120d786e4d81857e4b4dc2479412a7b38855
PADMANJALI <home@Homes-MacBook-Pro.local>
2022-12-01 16:35:27 -0800

    First version

---
+++ assignment/project.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$('#btn5').click(function(){
+
+//  $("#sp1").show()  
+ $('.progress').slideToggle()
+})
+$('#btn1').click(function(){
+
+    
+     $('#tb1').fadeToggle()
+    })
+
+
+
+})
